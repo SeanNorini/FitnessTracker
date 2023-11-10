6370aa780472e9f6dddadfc0334c05aadb53b86e
Sean Norini <141059916+SeanNorini@users.noreply.github.com>
2023-11-10 16:01:21 -0500

    Vercel troubleshooting

--- fitness_tracker/settings.py
+++ fitness_tracker/settings.py
@@ -129,13 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
-
-STATICFILES_DIRS = [
-  BASE_DIR / "static", 
-  BASE_DIR / "users/static",
-  BASE_DIR / "workout/static",
-]
+STATIC_URL = 'static/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
